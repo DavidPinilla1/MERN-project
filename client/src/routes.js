32ddd2be870d10b2d1f2231a2d9363a1b46ad1b1
David Pinilla <metroid_300@hotmail.com>
2018-12-07 22:17:39 +0100

    requiring auth in home path

--- client/src/routes.js
+++ client/src/routes.js
@@ -18,7 +18,7 @@ const Routes = () => {
   return (
     <Layout>
       <Switch>
-        <Route path="/" exact component={Auth(Home, null)} />
+        <Route path="/" exact component={Auth(Home, true)} />
         <Route path="/login" exact component={Auth(Login, false)} />
         <Route path="/user/logout" exact component={Auth(Logout, true)} />
         <Route path="/user" exact component={Auth(User, true)} />
